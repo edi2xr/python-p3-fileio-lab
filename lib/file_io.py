2325603208ce3f120c559536bb2d2b6a396d1655
edi2xr <mutuaadrian73@gmail.com>
2025-08-25 11:58:18 +0300

    submit

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
 def write_file(file_name, file_content):
-    pass
+    """Create or overwrite a .txt file with the given content."""
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    """Append content to an existing .txt file."""
+    with open(f"{file_name}.txt", "a") as f:
+        f.write(append_content)
+
 
 def read_file(file_name):
-    pass
+    """Read and return the content of a .txt file."""
+    with open(f"{file_name}.txt", "r") as f:
+        return f.read()
